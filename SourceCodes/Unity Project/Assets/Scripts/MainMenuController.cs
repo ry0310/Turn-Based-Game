331746a2c4c36c6d25201ed8917928772ed21572
ry0310 <ryan.tan0310@gmail.com>
2020-03-07 19:09:54 +0800

    Source Code

---
+++ SourceCodes/Unity Project/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public string sceneName, sceneName2;
+
+    public void StartGame() // Attached to the player button
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single); //Load 2 scenes, 1 as single and 1 as additive
+        SceneManager.LoadScene(sceneName2, LoadSceneMode.Additive);
+    }
+
+    public void ExitGame() // Attached to the exit game button
+    {
+        Application.Quit();
+    }
+}
